3c18a3a967f155c6e2b6f5b6a9e441ef52fe168c
Siyki <isabeta.mar@gmail.com>
2025-02-27 10:33:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,28 @@ st.image(fotito, caption= 'Sisoy')
 
 
 texto=st.text_input('Holi', 'holix2')
-st.write('El texto escrito es', texto)
+st.write('El texto escrito es:', texto)
 
 st.subheader ("Ahora usemos 2 columnas")
 
 col1,col2= st.columns(2)
+
+with col1:
+  st.subheader("Primera columna")
+  st.write("pros de diseño interactivo:")
+  resp= st.checkbox ("dibujitos")
+  if resp:
+    st.write("Obvio")
+
+with col2
+  st.subheader("Segunda columna")
+  modo= st.radio ("Razones por las que odiar la carrera",("nochamba", "quesehaceahi", "jesus"))
+if modo== "nochamba":
+  st.write("total, hay que salir de este hueco")
+
+if modo== "quesehaceahi":
+  st.write("diseño grafico 2.0)
+
+if modo== "jesus":
+  st.write("yo no le creo lo de rehabilitado")
+              
