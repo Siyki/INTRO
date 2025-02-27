95e88a2d8088359da63800d545b9fa427ef4b857
Siyki <isabeta.mar@gmail.com>
2025-02-27 10:20:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 st.title("mi primera chamba")
-st.header("holaholahola")
+st.header("la proxima semana no se como hice esto")
 st.write("soy diseñadora, no programadora, no sé que pretenden")
-image=Image.open('actually.jpg')
-st.image(image)
+fotito=Image.open('actually.jpg')
+st.image(fotito, caption= 'Sisoy')
