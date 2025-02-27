0ffb295bc2d0f5b80c9c5cf62b950b4d5730675b
Siyki <isabeta.mar@gmail.com>
2025-02-27 10:24:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import streamlit as st
 from PIL import Image
 
-st.title("mi primera chamba")
+st.title("Mi primera chamba")
 st.header("la proxima semana no se como hice esto")
 st.write("soy diseñadora, no programadora, no sé que pretenden")
 fotito=Image.open('actually.jpg')
 st.image(fotito, caption= 'Sisoy')
+
+
+texto=st.text_input('Holi', 'holix2')
+st.write('El texto escrito es', texto)
+
+st.subheader ("Ahora usemos 2 columnas")
+
+col1,col2= st.columns(2)
