d58bbf90cef00f56a85b172a0aa63f0c27cb8fff
Siyki <isabeta.mar@gmail.com>
2025-02-27 10:17:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,5 @@ from PIL import Image
 st.title("mi primera chamba")
 st.header("holaholahola")
 st.write("soy diseñadora, no programadora, no sé que pretenden")
-st.image=Image.open('actually.jpg')
-
+image=Image.open('actually.jpg')
 st.image(image)
